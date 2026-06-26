d3dcffda2f1289b41bdc0534394c8b8c0f0a056e
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-06-26 10:44:18 +0000

    user service and prisma added

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -6,10 +6,10 @@ const App = () => {
   }
 
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-background text-foreground">
       <header className="mx-auto flex max-w-7xl items-center justify-between px-6 py-6">
-        <div className="text-xl font-semibold tracking-tight text-slate-900">
-          Convo<span className="text-slate-500">fox</span>
+        <div className="text-xl font-semibold tracking-tight">
+          Convo<span className="text-muted-foreground">fox</span>
         </div>
 
         <Button onClick={handleLogin} className="rounded-full px-5">
@@ -18,16 +18,18 @@ const App = () => {
       </header>
 
       <main className="mx-auto flex max-w-4xl flex-col items-center px-6 pt-24 text-center">
-        <div className="mb-6 rounded-full border border-slate-200 bg-slate-50 px-4 py-1 text-sm text-slate-600">
+        <div className="mb-6 rounded-full border bg-muted px-4 py-1 text-sm text-muted-foreground">
           Secure Video Conferencing
         </div>
 
-        <h1 className="text-6xl font-semibold tracking-tight text-slate-900">
+        <h1 className="text-6xl font-semibold tracking-tight">
           Connect with anyone
-          <span className="block text-slate-500">from anywhere, instantly</span>
+          <span className="block text-muted-foreground">
+            from anywhere, instantly
+          </span>
         </h1>
 
-        <p className="mt-6 max-w-xl text-lg leading-8 text-slate-600">
+        <p className="mt-6 max-w-xl text-lg leading-8 text-muted-foreground">
           Host high-quality video meetings, collaborate in real time, share your
           screen, and stay connected with secure and reliable conferencing.
         </p>
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,11 +4,29 @@ import { createRoot } from "react-dom/client"
 import "@workspace/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 import App from "./App"
+import Dashboard from "@/routes/dashboard"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { createBrowserRouter, RouterProvider } from "react-router"
+
+const queryClient = new QueryClient()
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+  {
+    path: "/dashboard",
+    Component: Dashboard,
+  },
+])
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
 )
